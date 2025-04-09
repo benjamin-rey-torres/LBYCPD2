d04bc6cf8e7d01f5bbb6ebe649aeac30ff3a822a
benjamin-rey-torres <benjamin_torres@dlsu.edu.ph>
2025-04-09 15:23:08 +0800

    add password_verify stuff

--- employeeInformationMangement.py
+++ employeeInformationMangement.py
@@ -6,7 +6,7 @@ USE_SQL = False
 EMPLOYEE_CSV = "employeedata"
 
 
-def add_data(id,column,data,type=None):
+def add_employee_data(id,column,data,type=None):
     tempframe = dataImportAndExport.import_csv_to_dataframe(EMPLOYEE_CSV)
     tempframe.loc[id,column] = data
     print(tempframe)
@@ -19,6 +19,18 @@ def add_data(id,column,data,type=None):
 
 # def query_data():
 
-add_data(1,"salary",70000)
-add_data(1,"username","username123")
-add_data(1,"passwordHash",passwordHashing.returnHashPass("test123"))
+add_employee_data(1,"salary",70000)
+add_employee_data(1,"username","username123")
+add_employee_data(1,"passwordHash",passwordHashing.returnHashPass("test123"))
+add_employee_data(2,"salary",40000)
+add_employee_data(2,"username","username23")
+add_employee_data(2,"passwordHash",passwordHashing.returnHashPass("check1234"))
+add_employee_data(3,"salary",100000)
+add_employee_data(3,"username","username56")
+add_employee_data(3,"passwordHash",passwordHashing.returnHashPass("yes54321"))
+add_employee_data(4,"salary",10000)
+add_employee_data(4,"username","username78")
+add_employee_data(4,"passwordHash",passwordHashing.returnHashPass("password123"))
+add_employee_data(5,"salary",20000)
+add_employee_data(5,"username","username92")
+add_employee_data(5,"passwordHash",passwordHashing.returnHashPass(""))
