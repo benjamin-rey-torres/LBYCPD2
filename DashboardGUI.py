b783a0917f7fd71016e91451be48a76baf9f36d3
johnnque <johnn_patrick_que@dlsu.edu.ph>
2025-04-09 11:32:46 +0800

    Updated DashboardGUI
    added tabs
    attendance frame
    but buttons dont work yet

--- DashboardGUI.py
+++ DashboardGUI.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import datetime
+
 
 class DashboardWindow:
     def __init__(self):
@@ -10,33 +12,68 @@ class DashboardWindow:
         self.dashboardWindow.title("HR Managment Dashboard")
         self.dashboardWindow.config(background="white")
 
-        #Frame
+        #Main Frame
         self.dashboardFrame = Frame(self.dashboardWindow, bg="white")
         self.dashboardFrame.place(relx=0.5, rely=0.5, anchor="center")
 
-        #Buttons
-        self.payrollButton = Button(self.dashboardFrame, text='PAYROLL', command=self.showPayroll)
-        self.payrollButton.grid(row = 3, column =0) #CHANGE THIS
-
-        self.attendanceButton = Button(self.dashboardFrame, text='Attendance', command=self.showAttendanceGUI)
-        self.attendanceButton.grid(row = 3, column =1) #CHANGE THIS
-
         #Frames within dashboardFrame
+        #Tabs Frame
+        self.tabsFrame = Frame(self.dashboardFrame, bg="#f8fab4")
+        self.tabsFrame.config(width = 500, height = 300)
+        self.tabsFrame.pack(side = LEFT)
+
+        #Buttons in Tabs Frame
+        self.attendanceButton = Button(self.tabsFrame, text='Attendance', command=self.showAttendanceGUI)
+        self.attendanceButton.pack(side = TOP)
+
+        self.payrollButton = Button(self.tabsFrame, text='PAYROLL', command=self.showPayroll)
+        self.payrollButton.pack(side = TOP)
+
+        
         # Attendance Frame
         self.attendanceFrame = Frame(self.dashboardFrame, bg="#f8fab4")
-        self.attendanceFrame.grid(row = 0, column = 1, rowspan=2)
-
+        self.attendanceFrame.config(width=500, height=300)
+        self.attendanceFrame.pack(side = LEFT)
+        
+        # Attendance Frame Content
+        #TODO maybe add clock above th buttons
+        self.timeLabel = Label(self.attendanceFrame, text=datetime.datetime.now().strftime("%H:%M:%S"))
+        self.timeLabel.pack()
+        
+        self.updateButton = Button(self.attendanceFrame, text='UPDATE TIME', command = self.updateTime)
+        self.updateButton.pack()
+        self.timeInButton = Button(self.attendanceFrame,text='Time-In', command = self.timeIn)
+        self.timeInButton.pack()
+        self.timeOutButton = Button(self.attendanceFrame,text='Time-Out', command = self.timeOut)
+        self.timeOutButton.pack()
+        
         # Payroll Frame
         self.payrollFrame = Frame(self.dashboardFrame, bg="#f8fab4")
+        self.payrollFrame.config(width=500, height=300)
         
 
+
     def showPayroll(self):
-        self.payrollFrame.place(relx = 1, rely = 0,anchor = NE)
+        self.attendanceFrame.Place_forget()
+        self.payrollFrame.pack(side = LEFT)
+        
         print("show payroll")
 
     def showAttendanceGUI(self):
-        self.attendanceFrame.place(relx = 1, rely = 0,anchor = NE)
+        self.payrollFrame.Place_forget()
+        self.attendanceFrame.pack(side = LEFT)
         print("Attendance GUI")
+
+    def timeIn(self):
+        timeIn = datetime.datetime.now()
+
+    def timeOut(self):
+        timeOut = datetime.datetime.now()
+
+    def updateTime(self):
+        currentTime = datetime.datetime.now().strftime("%H:%M:%S")
+        self.time_var.set('Current Time: ' + currentTime)
+    
 
     def dashboardMainLoop(self):
         self.dashboardWindow.mainloop()
